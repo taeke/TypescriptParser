5a870c86e2a12a7952650cfcff405a7cf71a7315
taeke <taekevdv@gmail.com>
2015-09-20 20:46:50 +0200

    Code for creating chapter en rules in the typescript.pegjs file

--- src/js/generator.js
+++
@@ -1,8 +0,0 @@
-/// <reference path='./../../typings/tsd.d.ts' />
-'use strict';
-var fs = require('fs');
-var stream = fs.createWriteStream("./../typescript.pegjs");
-stream.once('open', function (fd) {
-    stream.write('// Typescript rules.');
-    stream.end();
-});
